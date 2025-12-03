2b56377b90e5c4ad37e7e4d5dbf0f603f300c33b
Mudasir Raza Attari <sheikh.mudasir11@yahoo.com>
2025-12-03 22:46:53 +0530

    strings

---
+++ 07_String number/conversion.js
@@ -0,0 +1,17 @@
+// ***********************operations*********************************
+
+let value = 3;
+let newValue = -value;
+
+// console.log(newValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Hitesh";
+let str2 = " Sir";
+ console.log(str1 + str2);
+ 
