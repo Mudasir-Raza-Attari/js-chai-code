482f81ca3e3194d0eae0f6eca9f7d485db477b6a
Mudasir Raza Attari <sheikh.mudasir11@yahoo.com>
2025-12-11 15:59:29 +0530

    objects_03

--- 17_Objects_P2/Objects_two.js
+++ 17_Objects_P2/Objects_two.js
@@ -1,1 +1,30 @@
 // const tinderUser = new Object()
+
+// const tinderUser = {};
+
+// tinder;
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "c", 4: "d" };
+const obj3 = { 5: "e", 6: "f" };
+
+// const obj4 = {obj1 ,obj2} // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+// console.log(obj4);
+
+// const obj5 = Object.assign({}, obj1, obj2); //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+// console.log(obj5);
+
+const obj5 = { ...obj1, ...obj2, ...obj3 }; //{ '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+console.log(obj5); // mostly used method
+
+const users = [
+  {
+    id: 1,
+    email: "h@gmil.com",
+  },
+  {},
+  {},
+];
+
+const one = users[1].email;
+console.log(one);
---
+++ 18_Objects_P3/object_De-structure.js
@@ -0,0 +1,17 @@
+const course = {
+  coursename: "Js in hindi",
+  price: "999",
+  Instructor: "Hitesh",
+};
+// course.coursename
+
+const { Instructor } = course;
+console.log(Instructor);
+
+// +++++++++API++++++++++
+
+// {
+//     'name':'hitesh',
+//     'courseName': 'Js in himdi',
+//     'price':'free'
+// }
