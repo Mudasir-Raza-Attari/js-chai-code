c5fc8a061c1ee044ef950b24af4102be3827ab8f
Mudasir Raza Attari <sheikh.mudasir11@yahoo.com>
2025-12-14 21:27:01 +0530

    THIS & Arrow Function

---
+++ 23_THIS and arrow Function/arrowFunction.js
@@ -0,0 +1,9 @@
+const user = {
+  username: "Hitesh",
+  price: 999,
+
+  welcomeMessage: function () {
+    console.log(`${this.username}, welcome to website`);
+  },
+};
+username.welcomeMessage();
