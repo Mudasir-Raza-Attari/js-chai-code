6f856656954cc194cf60e48b8c9a4ff547e8cd24
Mudasir Raza Attari <sheikh.mudasir11@yahoo.com>
2025-12-16 17:04:50 +0530

    control Flow

---
+++ 26_Control Flow/one.js
@@ -0,0 +1,32 @@
+// IF
+
+if (true) {
+  // console.log("Hi Janu");
+}
+const isUserLogedIn = true;
+if (isUserLogedIn) {
+  // console.log("I love you");
+}
+
+// <,>,<=,>=,==,!==,===,!==
+
+// const balance = 1000;
+
+// if (balance > 500) console.log("test");
+
+// if (balance < 500) {
+//   console.log('less than');
+// }else if (balance < 750){
+//   console.log('less than');
+
+// }else (balance < 750); {
+//   console.log('mew mew');
+
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+
+if (userLoggedIn && debitCard) {
+  console.log("Allow to buy course");
+}
---
+++ 26_Control Flow/truthy&falsy.js
@@ -0,0 +1,25 @@
+// const userEmail = "hitesh@gmail"; //       Got user Email
+// const userEmail = ""; //         Dont have user email
+// const userEmail = "hitesh@gmail"; //       Got user Email
+// const userEmail = []  //                   Got user Email
+const userEmail = [];
+
+if (userEmail) {
+  console.log("Got user Email");
+} else {
+  console.log("Dont have user email");
+}
+
+// Falsy values
+
+// false, 0, -0, BigInt, "", null, undefined, NaN;
+
+// Truthy values
+
+// true, 1, "0", "false", " ", [], {}, function () {};
+
+
+
+// Ternary Operators
+
+// condition ? true : false
