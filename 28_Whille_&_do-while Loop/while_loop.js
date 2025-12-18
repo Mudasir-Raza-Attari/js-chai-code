e51353e91036add4cb7b240269e3703929f3dc8f
Mudasir Raza Attari <sheikh.mudasir11@yahoo.com>
2025-12-18 14:38:12 +0530

    Loops

---
+++ 28_Whille_&_do-while Loop/while_loop.js
@@ -0,0 +1,24 @@
+let index = 0;
+while (index <= 10) {
+  console.log(`Value of the Index is ${index}`);
+  index = index + 2;
+}
+
+// ...............................................
+
+let myarray = ["flash", "batman", "shaktiman"];
+
+let arr = 0;
+
+while (arr < myarray.length) {
+  console.log(`heroes are here: ${myarray[arr]}`);
+  arr++;
+}
+
+//,...................................................
+
+let score = 1;
+do {
+  console.log(`score is ${score}`);
+  score++;
+} while (score <= 10);
---
+++ 29_High_Order_Array_Loops/for_each_loop.js
@@ -0,0 +1,27 @@
+const coding = ["js", "ruby", "java", "python", "c++"];
+
+// function
+// coding.forEach(function (item) {
+// console.log(item);
+// });
+
+//array
+coding.forEach((val) => {
+  //   console.log(`val of ${val}`);
+});
+
+function printme(item) {
+  console.log(item);
+}
+// coding.forEach(printme);
+
+// ...................................
+
+const mycoding = [
+  { className: "OOla", noOfStudents: 36 },
+  { className: "saniya", noOfStudents: 41 },
+  { className: "Rabiya", noOfStudents: 63 },
+];
+mycoding.forEach((item) => {
+  console.log(item.className);
+});
