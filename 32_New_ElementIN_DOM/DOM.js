d9fc1d2870483bd84b11b97196a39f683539d08f
Mudasir Raza Attari <sheikh.mudasir11@yahoo.com>
2025-12-21 16:30:42 +0530

    DOM adv

---
+++ 32_New_ElementIN_DOM/DOM.js
@@ -0,0 +1,26 @@
+// document.querySelector(".parent");
+const parent = document.querySelector(".parent");
+// console.log(parent);
+// console.log(parent.children);
+// console.log(parent.children[1]);
+// console.log(parent.children[1].innerHTML);
+
+// for (let i = 0; i < parent.children.length; i++) {
+//   console.log(parent.children[i].innerHTML);
+// }
+
+// console.log(parent);
+// console.log(parent.children);
+// console.log(parent.children[0]);
+
+// parent.children[1].style.color = 'orange'
+
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+const dayOne = document.querySelector(".day");
+console.log(dayOne);
+console.log(dayOne.parentElement);
+console.log(dayOne.nextElementSibling);
+
+console.log("NODE:", parent.childNodes);
